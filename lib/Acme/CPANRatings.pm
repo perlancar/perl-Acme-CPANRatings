81cd45f3f8abf8b7bf662b67a8c41d919e2b2f1a
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-05-09 19:23:58 +0700

    Initial commit

---
+++ lib/Acme/CPANRatings.pm
@@ -0,0 +1,13 @@
+package Acme::CPANRatings;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: A dummy module just so we can rate CPAN Ratings (the site itself)
+
+=head1 SEE ALSO
+
+L<https://cpanratings.perl.org/>
+
+=cut
